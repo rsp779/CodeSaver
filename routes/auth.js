836790028a84e9bba17398f479d1c2c7e840bfc9
Rana Suraj Pratap <50663242+rsp779@users.noreply.github.com>
2022-06-26 22:58:03 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,3 +9,7 @@ router.route("/login").post(login);
 router.route("/forgotpassword").post(forgotpassword);
 router.route("/resetpassword/:resetToken").put(resetpassword);
 module.exports=router;
+
+
+// here the various types of method are specified to various routes 
+// that is register,login and forgotpassword would be post request and reset password will be put request.
